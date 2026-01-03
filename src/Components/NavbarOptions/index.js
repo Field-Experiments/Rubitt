a01abf5f9fbe83ed833657c8ed799dd3141dfd10
Vishnuvardhan-svv <s.vishnu1105@gmail.com>
2026-01-03 11:07:31 +0530

    frontend

---
+++ src/Components/NavbarOptions/index.js
@@ -0,0 +1,12 @@
+import './index.css'
+const Options = () => {
+  return (
+    <ul className="nav-links">
+        <li>Home</li>
+        <li>About</li>
+        <li>Reports</li>
+        <li>Contact</li>
+      </ul>
+    )
+}
+export default Options
