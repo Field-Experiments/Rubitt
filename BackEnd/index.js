d707bd02b8efd8aba927623b781168f689b73789
Rishi00000 <rishidhar66@gmail.com>
2026-01-24 10:57:27 +0530

    Installed Dependencies

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,1 +1,12 @@
-console.log("BackEnd index.js file loaded");
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
+});
